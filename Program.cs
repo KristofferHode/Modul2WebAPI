e1b29dc0bef1becc65826807cc1b96822ab8722c
KristofferHode <kristoffer9.kodehode@gmail.com>
2026-01-08 09:09:47 +0100

    Swagger

--- Program.cs
+++ Program.cs
@@ -1,17 +1,21 @@
 using Modul2WEBAPI;
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
 builder.Services.AddSingleton<VideoGameCsvRepository>();
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSingleton<VideoGameCsvRepository>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
+
 app.MapControllers();
 
 app.Run();
